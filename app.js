205087a4157faf2e5590c87146b3ff63dd8fdad6
Ayoub24 <ayoub.anwar24@mail.com>
2018-10-06 14:34:05 -0700

    inputs controle

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 function myFunction() {
     var FName = document.getElementById('FirstName').value;
-    var LName = document.getElementById('LastName').value;
-    
-    if (FName !== "" && LName !== "") {
-	   document.querySelector('.bg-modal').style.display = "flex";
-       console.log(FName);
-       document.getElementById("full_name").innerHTML = FName +" "+ LName;
-    }    
+    var LName = document.getElementById('LastName').value;   
+    document.querySelector('.bg-modal').style.display = "flex";
+    document.getElementById("full_name").innerHTML = FName +" "+ LName;      
 }
 
 function myClose() {
     document.querySelector('.bg-modal').style.display = "none";
+}
+
+$(document).ready(function (){
+    validate();
+    $('#FirstName, #LastName').change(validate);
+});
+
+function validate(){
+    if ($('#FirstName').val().length   >   4   &&
+        $('#LastName').val().length  >   4 ) {
+        $("input[type=button]").prop("disabled", false);
+    }
+    else {
+        $("input[type=button]").prop("disabled", true);
+    }
 }
